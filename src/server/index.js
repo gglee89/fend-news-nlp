9ee5c3472d916192ca290079b8335d8848ab6b75
Gustavo Lee <gglee89@gmail.com>
2020-09-07 17:11:34 -0400

    feat: Enable service-worker for production

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,20 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 8080;
 
-var json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
-}
-
-const app = express()
-app.use(cors())
-// to use json
-app.use(bodyParser.json())
-// to use url encoded values
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
-}))
+    extended: true
+}));
+app.use(express.static('dist'));
 
-app.use(express.static('dist'))
-
-console.log(JSON.stringify(mockAPIResponse))
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
-})
+});
 
-app.get('/test', function (req, res) {
-    res.json(mockAPIResponse);
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 })
