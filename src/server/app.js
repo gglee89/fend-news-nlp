9a16060cec38ace2ddec21727a7d7b9f01babd5f
Gustavo Lee <gglee89@gmail.com>
2020-09-10 15:00:36 -0400

    feat: Include express static 'dist'

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,23 @@
 /**
  * APP FILE
  */
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const mockData = require('./mockData');
 
+// MIDDLEWARE
+app.use(express.static('dist'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// ENDPOINTS
 app.get('/', (req, res) => {
   res.status(200).sendFile('dist/index.html');
 });
--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,9 @@
 /**
  * SERVER FILE
  */
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const PORT = 8080;
 
 const app = require('./app');
-app.use(cors());
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
